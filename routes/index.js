449f8656c482e9f7747200c56c9877dc9b864a70
viswadeepthi <vrejinthala@gainsight.com>
2018-03-05 22:44:38 +0530

    list changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
+
 var express = require('express');
 var router = express.Router();
-
+var {Contacts, Calls} = require('../db/models/index');
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+
+
+  var callsList = await Calls.findAll();
+  callsList = JSON.parse(JSON.stringify(callsList));
+  var contactsList = await Contacts.findAll();
+  contactsList = JSON.parse(JSON.stringify(contactsList));
+
+
+  callsList.forEach(function(call){
+    call.contacts = contactsList.filter(function(contact){
+      return contact.call_id === call.id;
+    });
+    console.log(call);
+  });
+ 
+  res.render('index', { title: 'Express', callsList: JSON.parse(JSON.stringify(callsList)) });
 });
 
 module.exports = router;
